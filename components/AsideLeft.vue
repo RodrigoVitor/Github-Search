2977341fc35ed27df27b0535a1a6b47d14ca0782
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2022-05-08 15:36:01 -0300

    get data users

--- components/AsideLeft.vue
+++ components/AsideLeft.vue
@@ -1,23 +1,32 @@
 <template>
-    <div id="user">
+    <div id="user" v-if="user">
         <aside class="user">
             <figure>
-                <b-img src="https://avatars.githubusercontent.com/u/77356444?v=4" alt="Uma imagem impressionante"></b-img>
+                <b-img :src="user.avatar_url" width="200" alt="Uma imagem impressionante"></b-img>
                 <header>
-                    <p>Rodrigo Vitor <small>Rodrigo Vitor</small> </p>
+                    <p>{{user.login}} <small>{{user.bio}}</small> </p>
                     <!-- <p></p> -->
                 </header>
             </figure>
             <div>
-                <p><b-img src="/icon/union.png" fluid alt="localização"></b-img> Lux One</p>
-                <p><b-img src="/icon/location.png" fluid alt="localização"></b-img> Localização</p>
+                <p v-if="user.company"><b-img src="/icon/union.png" fluid alt="localização"></b-img> {{user.company}}</p>
+                <p v-if="user.location"><b-img src="/icon/location.png" fluid alt="localização"></b-img> {{user.location}}</p>
                 <p><b-img src="/icon/star.png" fluid alt="estrelas"></b-img> 0</p>
-                <p><b-img src="/icon/repository.png" fluid alt="repositorios"></b-img>1</p>
+                <p><b-img src="/icon/repository.png" fluid alt="repositorios"></b-img>{{user.public_repos}}</p>
                 <p><b-img src="/icon/followers.png" fluid alt="seguidores"></b-img>14</p>
             </div>
         </aside>
     </div>
 </template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+    computed: {
+        ...mapState('search', ['user'])
+    }
+}
+</script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@1,300&display=swap');
@@ -35,9 +44,7 @@ aside figure header p {
 aside figure header p > small{
     font-size:24px;
     display:block;
-    line-height: 10px;
     margin-bottom:72px;
-
 }
 aside div p {
     font-size:20px;
